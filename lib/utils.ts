722673d1c5b9b9d84c950d1fcee9d9157e13d30b
Tomarun029831 <st22130kk@gm.ibaraki-ct.ac.jp>
2025-09-06 16:58:16 +0900

    feat(utils): add new function to format time(HH:MM:SS -> HH:MM)

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,5 +2,13 @@ import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs))
+}
+
+export function timeFormatJPHM(time: Date): string {
+    return new Intl.DateTimeFormat("ja-JP", {
+        hour: "2-digit",
+        minute: "2-digit",
+        hour12: false,
+    }).format(time)
 }
