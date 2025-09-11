270a25fc8bfbd7cf09117c33b70893b1796efbd1
Tomarun029831 <st22130kk@gm.ibaraki-ct.ac.jp>
2025-09-11 23:25:11 +0900

    fix(middleware): revise if-statement to correctly work

--- middleware.ts
+++ middleware.ts
@@ -1,27 +1,28 @@
 import { NextResponse } from "next/server"
-import { NextRequest } from "next/server"
+import type { NextRequest } from "next/server"
 
 interface APICheckTokenResponse { success: boolean }
 
-const directoryToSeekToken: string[] = ['/admin'];
+const protectedPaths = ['/admin']
 
 export async function middleware(req: NextRequest) {
     const token = req.cookies.get("token")?.value;
-    if (token === undefined || directoryToSeekToken.some(s => req.nextUrl.pathname.startsWith(s)))
-        return NextResponse.redirect(new URL('/', req.url));
 
-    const query: string = "?action=checkToken"; // HACK:
-    const res = await fetch(process.env.GAS_URL + query as string, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' }, // ブラウザから送るわけではないのでCORS対策は必要ない
-        body: JSON.stringify({ 'token': token })
-    });
-    const data: APICheckTokenResponse = await res.json();
+    if (!token) return NextResponse.redirect(new URL('/', req.url))
 
-    if (data.success)
-        return NextResponse.next();
+    if (protectedPaths.some(p => req.nextUrl.pathname.startsWith(p))) {
+        const query = "?action=checkToken"
+        const res = await fetch(`${process.env.GAS_URL}${query}`, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ token })
+        })
+        const data: APICheckTokenResponse = await res.json()
 
-    return NextResponse.redirect(new URL('/', req.url));
+        if (!data.success) return NextResponse.redirect(new URL('/', req.url))
+    }
+
+    return NextResponse.next()
 }
 
 export const config = {
